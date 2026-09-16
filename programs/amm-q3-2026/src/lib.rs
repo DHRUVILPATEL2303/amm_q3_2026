fe4eb8fd4d7248335eee73d8fdd96441569fd882
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-09-16 13:59:00 +0530

    feat : deposit

--- programs/amm-q3-2026/src/lib.rs
+++ programs/amm-q3-2026/src/lib.rs
@@ -18,4 +18,8 @@ pub mod amm_q3_2026 {
     pub fn initalize(ctx : Context<Initialize>,seed : u64,fee : u16,authority : Option<Pubkey>) -> Result<()>{
         ctx.accounts.init(seed, fee, authority, ctx.bumps)
     }
+
+    pub fn deposit(ctx : Context<Deposit> ,amount: u64,   max_x: u64,  max_y: u64  ) -> Result<()>{
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
 }
